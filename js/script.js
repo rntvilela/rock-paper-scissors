ec57cbb9d56faddc1b79e23bda5dcaab612712e5
rntvilela <renatovilela55@gmail.com>
2023-04-22 00:30:17 -0300

    UI

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
+let playerPoints = 0;
+let computerPoints = 0;
+let round = 0;
+let result_list = [];
+
+document.addEventListener('DOMContentLoaded', function() {
+
+	const button_rock     = document.getElementById('rock');
+	const button_paper    = document.getElementById('paper');
+	const button_scissors = document.getElementById('scissors');
+
+	const result = document.getElementById('result');
+	const final_result = document.getElementById('final');
+
+	button_rock.addEventListener('click', function() {
+		let list_result = game('Rock');	
+		result.textContent = list_result[0]
+		final_result.textContent = list_result[1];
+		result_list = ["", ""];
+	});
+	
+	button_paper.addEventListener('click', function() {
+		let list_result = game('Paper');	
+		result.textContent = list_result[0]	
+		final_result.textContent = list_result[1];
+		result_list = ["", ""];
+	});
+
+	button_scissors.addEventListener('click', function() {
+		let list_result = game('Scissors');	
+		result.textContent = list_result[0]	
+		final_result.textContent = list_result[1];
+		result_list = ["", ""];
+	});
+	
+});
+
+
 function computerPlay() {
     const a = ['Rock', 'Paper', 'Scissors'];
     return a[Math.floor(Math.random() * 3)];
@@ -10,58 +48,58 @@ function playRound(playerSelection, computerSelection) {
         if(computerSelection == 'Rock') {
             return "Tie!";
         } else if (computerSelection == 'Paper') {
-            return "You Lose! Paper beats Rock";
+            return "You Lose! Paper beats Rock.";
         } else {
-            return "You Won! Rock beats Scissors";
+            return "You Won! Rock beats Scissors.";
         }
     } else if(playerSelection == 'Paper') {
         if(computerSelection == 'Rock') {
-            return "You Won! Paper beats Rock";
+            return "You Won! Paper beats Rock.";
         } else if (computerSelection == 'Paper') {
             return "Tie!";
         } else {
-            return "You Lose! Scissors beats Paper";
+            return "You Lose! Scissors beats Paper.";
         }
     } else {
         if(computerSelection == 'Rock') {
-            return "You Lose! Rock beats Scissors";
+            return "You Lose! Rock beats Scissors.";
         } else if (computerSelection == 'Paper') {
-            return "You Won! Scissors beats Paper";
+            return "You Won! Scissors beats Paper.";
         } else {
             return "Tie!";
         }
     }
 }
 
-function game() {
-    let playerPoints = 0;
-    let computerPoints = 0;
+function game(playerSelection) {
+	let computerSelection = ''; 
+    let result = '';
 
-    let playerSelection = ''; 
-    let computerSelection = ''; 
-    let result = ''; 
+    computerSelection = computerPlay();
+    result = playRound(playerSelection, computerSelection);    
+	result_list[0] = result;
 
-    for(let i = 0; i < 5; i++) {
-        playerSelection = prompt("Rock or Paper or Scissors?");
-        computerSelection = computerPlay();
-        result = playRound(playerSelection, computerSelection);    
-
-        console.log(result); 
-
-        if(result.slice(0, 7) == 'You Won') {
-            playerPoints++;
-        } else if(result.slice(0, 8) == 'You Lose') {
-            computerPoints++;
-        }
+    if(result.slice(0, 7) == 'You Won') {
+		playerPoints++;
+	} else if(result.slice(0, 8) == 'You Lose') {
+        computerPoints++;
     }
 
-    if(playerPoints > computerPoints) {
-        console.log("You Won the Game! You: " + playerPoints + " - Computer: " + computerPoints);
-    } else if(playerPoints < computerPoints) {
-        console.log("You Lose the Game! You: " + playerPoints + " - Computer: " + computerPoints);
-    } else {
-        console.log("Tie! You: " + playerPoints + " - Computer: " + computerPoints);
-    }
+	round++;
+
+	if (round == 3) {
+		if(playerPoints > computerPoints) {
+			result_list[1] = "You Won the Game! You: " + playerPoints + " - Computer: " + computerPoints;
+		} else if(playerPoints < computerPoints) {
+			result_list[1] = "You Lose the Game! You: " + playerPoints + " - Computer: " + computerPoints;
+		} else {
+			result_list[1] = "Tie! You: " + playerPoints + " - Computer: " + computerPoints;
+		}
+		
+		playerPoints = 0;
+		computerPoints = 0;
+		round = 0;
+	}
+	
+	return result_list;
 }
-
-game();
